741cee4b37932a357035bb22bf7c3901522110dd
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-09-27 15:56:29 +0300

    test product image gallery list

---
+++ tests/components/ProductImageGallery.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import ProductImageGallery from "../../src/components/ProductImageGallery";
+
+describe("ProductImageGallery", () => {
+  it("should render null when the image gallery array is empty", () => {
+    const { container } = render(<ProductImageGallery imageUrls={[]} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it("should render list of image urls", () => {
+    const imageUrls: string[] = ["url", "url2"];
+
+    render(<ProductImageGallery imageUrls={imageUrls} />);
+
+    const images = screen.getAllByRole("img");
+    expect(images).toHaveLength(2);
+    imageUrls.forEach((url, index) => {
+      expect(images[index]).toHaveAttribute("src", url);
+    });
+  });
+});
