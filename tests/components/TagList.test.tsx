1ebfb0a2a3d00ce3d18fd59adcc767af4f9d2a92
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-10-10 15:02:47 +0300

    testing asynchronous code

---
+++ tests/components/TagList.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import TagList from "../../src/components/TagList";
+
+describe("TagList", () => {
+  it("should render at least on list item", async () => {
+    render(<TagList />);
+    await waitFor(() => {
+      const listItem = screen.getAllByRole("listitem");
+      expect(listItem.length).toBeGreaterThan(0);
+    });
+  });
+});
