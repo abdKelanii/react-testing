86b98e3e3df33df97a6949a5c07ccb5b0e84c339
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-10-30 14:27:55 +0300

    testing loading skeleton

---
+++ tests/components/BrowseProductPage.test.tsx
@@ -0,0 +1,69 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { http, delay, HttpResponse } from "msw";
+import { server } from "../mocks/server";
+import BrowseProducts from "../../src/pages/BrowseProductsPage";
+import { Theme } from "@radix-ui/themes";
+
+describe("BrowseProductPage", () => {
+  const renderComponent = () => {
+    render(
+      <Theme>
+        <BrowseProducts />
+      </Theme>
+    );
+  };
+
+  it("should show loading skeleton when fetching categories", () => {
+    server.use(
+      http.get("/categories", async () => {
+        await delay();
+        return HttpResponse.json([]);
+      })
+    );
+
+    renderComponent();
+
+    screen.debug();
+
+    expect(
+      screen.getByRole("progressbar", { name: /categories/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should hide the loading skeleton after categories are fetched"),
+    async () => {
+      renderComponent();
+      await waitForElementToBeRemoved(() =>
+        screen.getByRole("progressbar", { name: /categories/i })
+      );
+    };
+
+  it("should show loading skeleton when fetching products", () => {
+    server.use(
+      http.get("/products", async () => {
+        await delay();
+        return HttpResponse.json([]);
+      })
+    );
+
+    renderComponent();
+
+    screen.debug();
+
+    expect(
+      screen.getByRole("progressbar", { name: /products/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should hide the loading skeleton after products are fetched"),
+    async () => {
+      renderComponent();
+      await waitForElementToBeRemoved(() =>
+        screen.getByRole("progressbar", { name: /products/i })
+      );
+    };
+});
