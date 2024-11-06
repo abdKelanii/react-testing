dd85196e2d522aa79b85e39cd80a96e11a65bcca
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-11-07 01:25:04 +0300

    testing validation errors

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,10 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ProductForm from "../../src/components/ProductForm";
 import { Category, Product } from "../../src/entities";
 import AllProviders from "../AllProviders";
 import { db } from "../mocks/db";
-import userEvent from "@testing-library/user-event";
-import { error } from "console";
 
 describe("ProductForm", () => {
   let category: Category;
@@ -89,6 +88,47 @@ describe("ProductForm", () => {
       const user = userEvent.setup();
       if (name !== undefined) await user.type(form.nameInput, name);
       await user.type(form.priceInput, "10");
+      await user.click(form.categoryInput);
+      const options = screen.getAllByRole("option");
+      await user.click(options[0]);
+      await user.click(form.submitButton);
+
+      const error = screen.getByRole("alert");
+      expect(error).toBeInTheDocument();
+      expect(error).toHaveTextContent(errorMessage);
+    }
+  );
+
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "0",
+      price: 0,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "negative",
+      price: -1,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "greater than 1000",
+      price: 1001,
+      errorMessage: /1000/,
+    },
+  ])(
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad } = renderForm();
+      const form = await waitForFormToLoad();
+
+      const user = userEvent.setup();
+      await user.type(form.nameInput, "a");
+      if (price !== undefined)
+        await user.type(form.priceInput, price.toString());
       await user.click(form.categoryInput);
       const options = screen.getAllByRole("option");
       await user.click(options[0]);
