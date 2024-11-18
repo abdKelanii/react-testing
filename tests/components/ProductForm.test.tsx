52024c8f9c25e4dc3d4612543ea201220cb43ed1
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-11-18 08:39:48 +0300

    extracting a function for filling forms

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -21,15 +21,53 @@ describe("ProductForm", () => {
     });
 
     return {
+      expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
+        const error = screen.getByRole("alert");
+        expect(error).toBeInTheDocument();
+        expect(error).toHaveTextContent(errorMessage);
+      },
       waitForFormToLoad: async () => {
         await screen.findByRole("form");
+        const nameInput = screen.getByPlaceholderText(/name/i);
+        const priceInput = screen.getByPlaceholderText(/price/i);
+        const submitButton = screen.getByRole("button");
+        const categoryInput = screen.getByRole("combobox", {
+          name: /category/i,
+        });
+
+        type formData = {
+          [K in keyof Product]: any;
+        };
+        const validData: formData = {
+          id: 1,
+          name: "Product 1",
+          price: 100,
+          categoryId: category.id,
+        };
+
+        const fill = async (product: formData) => {
+          const user = userEvent.setup();
+
+          if (product.name !== undefined)
+            await user.type(nameInput, product.name);
+
+          if (product.price !== undefined)
+            await user.type(priceInput, product.price.toString());
+
+          await user.tab();
+          await user.click(categoryInput);
+          const options = screen.getAllByRole("option");
+          await user.click(options[0]);
+          await user.click(submitButton);
+        };
+
         return {
-          nameInput: screen.getByPlaceholderText(/name/i) as HTMLInputElement,
-          priceInput: screen.getByPlaceholderText(/price/i) as HTMLInputElement,
-          categoryInput: screen.getByRole("combobox", {
-            name: /category/i,
-          }) as HTMLSelectElement,
-          submitButton: screen.getByRole("button"),
+          nameInput,
+          priceInput,
+          categoryInput,
+          submitButton,
+          fill,
+          validData,
         };
       },
     };
@@ -69,7 +107,7 @@ describe("ProductForm", () => {
     expect(nameInput).toHaveFocus();
   });
 
-  it.each([
+  it.only.each([
     {
       scenario: "missing",
       errorMessage: /required/i,
@@ -82,20 +120,12 @@ describe("ProductForm", () => {
   ])(
     "should display an error if name is $scenario",
     async ({ name, errorMessage }) => {
-      const { waitForFormToLoad } = renderForm();
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } = renderForm();
+
       const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, name });
 
-      const user = userEvent.setup();
-      if (name !== undefined) await user.type(form.nameInput, name);
-      await user.type(form.priceInput, "10");
-      await user.click(form.categoryInput);
-      const options = screen.getAllByRole("option");
-      await user.click(options[0]);
-      await user.click(form.submitButton);
-
-      const error = screen.getByRole("alert");
-      expect(error).toBeInTheDocument();
-      expect(error).toHaveTextContent(errorMessage);
+      expectErrorToBeInTheDocument(errorMessage);
     }
   );
 
@@ -122,21 +152,11 @@ describe("ProductForm", () => {
   ])(
     "should display an error if price is $scenario",
     async ({ price, errorMessage }) => {
-      const { waitForFormToLoad } = renderForm();
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } = renderForm();
       const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, price });
 
-      const user = userEvent.setup();
-      await user.type(form.nameInput, "a");
-      if (price !== undefined)
-        await user.type(form.priceInput, price.toString());
-      await user.click(form.categoryInput);
-      const options = screen.getAllByRole("option");
-      await user.click(options[0]);
-      await user.click(form.submitButton);
-
-      const error = screen.getByRole("alert");
-      expect(error).toBeInTheDocument();
-      expect(error).toHaveTextContent(errorMessage);
+      expectErrorToBeInTheDocument(errorMessage);
     }
   );
 });
